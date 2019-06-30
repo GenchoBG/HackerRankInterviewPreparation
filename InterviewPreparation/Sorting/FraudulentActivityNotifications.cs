69b7032def873e72085f4e083d4fdff688d0d39d
GenchoBG <genchobg11@gmail.com>
2019-06-30 22:19:27 +0300

    Add Fraudulent Activity Notifications proper solution

--- InterviewPreparation/Sorting/FraudulentActivityNotifications.cs
+++ InterviewPreparation/Sorting/FraudulentActivityNotifications.cs
@@ -13,21 +13,54 @@ namespace Sorting
             var n = firstLineData[0];
             var trailing = firstLineData[1];
 
-            var days = Console.ReadLine().Split().Select(double.Parse).ToArray();
-
-            var orderedDays = days.OrderBy(d => d).ToArray();
+            var days = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
             var count = 0;
 
+            var countingSort = new int[201];
+            for (int i = 0; i < trailing; i++)
+            {
+                countingSort[days[i]]++;
+            }
+
+            var middleIndex = (trailing / 2) + 1;
+
             for (int day = trailing; day < days.Length; day++)
             {
-                if (days[day] < orderedDays[day])
+                var median = FindAtIndex(countingSort, middleIndex);
+                if (trailing % 2 == 0)
+                {
+                    median += FindAtIndex(countingSort, middleIndex - 1);
+                    median /= 2;
+                }
+
+                var current = days[day];
+                var last = days[day - trailing];
+
+                if (current >= median * 2)
                 {
                     count++;
                 }
+
+                countingSort[current]++;
+                countingSort[last]--;
             }
 
             Console.WriteLine(count);
+        }
+
+        private static double FindAtIndex(int[] frequencies, int index)
+        {
+            var count = 0;
+
+            for (int i = 0; ; i++)
+            {
+                count += frequencies[i];
+                if (count >= index)
+                {
+                    return i;
+                }
+            }
         }
     }
 }
