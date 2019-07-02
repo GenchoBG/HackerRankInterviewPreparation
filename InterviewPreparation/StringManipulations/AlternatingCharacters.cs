f5e381f724b63f9d7d605153094e173ae8574b4f
GenchoBG <genchobg11@gmail.com>
2019-07-02 23:07:46 +0300

    Add Alternating Characters & Valid Strings solutions

---
+++ InterviewPreparation/StringManipulations/AlternatingCharacters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringManipulations
+{
+    //https://www.hackerrank.com/challenges/alternating-characters/problem
+    public class AlternatingCharacters
+    {
+        public static void Main()
+        {
+            var q = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < q; i++)
+            {
+                var input = Console.ReadLine();
+
+                var count = 0;
+
+                for (int j = 1; j < input.Length; j++)
+                {
+                    if (input[j] == input[j - 1])
+                    {
+                        count++;
+                    }
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
---
+++ InterviewPreparation/StringManipulations/ValidStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace StringManipulations
+{
+    //https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+    public class ValidStrings
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var occurences = input.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+
+            var values = occurences.Values.Distinct().ToList();
+
+            // if all the characters occur equaly
+            var isValid = values.Count == 1;
+
+            if (values.Count == 2)
+            {
+                var first = values[0];
+                var firstChars = occurences.Count(kvp => kvp.Value == first);
+                var second = values[1];
+                var secondChars = occurences.Count(kvp => kvp.Value == second);
+
+                // if there is exactly 1 character occuring exactly 1 time more than the rest of the character
+                if ((Math.Abs(first - second) == 1 && (firstChars == 1 || secondChars == 1)))
+                {
+                    isValid = true;
+                }
+
+                // if there is exactly 1 character occuring 1 time we can remove it
+                if ((firstChars == 1 && first == 1) || (secondChars == 1 && second == 1))
+                {
+                    isValid = true;
+                }
+            }
+
+            Console.WriteLine(isValid ? "YES" : "NO");
+        }
+    }
+}
