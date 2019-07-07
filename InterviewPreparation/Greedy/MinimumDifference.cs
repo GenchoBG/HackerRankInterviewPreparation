5444c4be93ade637304d375528cf267be350094e
GenchoBG <genchobg11@gmail.com>
2019-07-07 22:54:08 +0300

    Add Minimum Difference solution

---
+++ InterviewPreparation/Greedy/MinimumDifference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Greedy
+{
+    //https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+    public class MinimumDifference
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var elements = Console.ReadLine().Split().Select(int.Parse).OrderBy(e => e).ToArray();
+
+            var min = int.MaxValue;
+            for (int i = 0; i < n - 1; i++)
+            {
+                var current = Math.Abs(elements[i] - elements[i + 1]);
+
+                if (current < min)
+                {
+                    min = current;
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
